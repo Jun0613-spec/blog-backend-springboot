04b81820a2f76ac2218458e48b6ad857417a8045
Jun0613-spec <kumo5110@gmail.com>
2024-02-01 14:53:17 +0000

    updated

--- src/main/java/com/jun/blog/BackendApplication.java
+++ src/main/java/com/jun/blog/BackendApplication.java
@@ -2,10 +2,8 @@ package com.jun.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.jun.blog.jwt")
 public class BackendApplication {
 
 	public static void main(String[] args) {
