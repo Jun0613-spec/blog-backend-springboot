3888261f0c9ede7e65376f4cddcd9598c846dd8c
Jun0613-spec <kumo5110@gmail.com>
2024-02-02 16:25:57 +0000

    security edited

--- src/main/java/com/jun/blog/config/WebSecurityConfig.java
+++ src/main/java/com/jun/blog/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig{
     .httpBasic(httpBasic -> httpBasic.disable())
     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     .authorizeHttpRequests(request -> request
-            .requestMatchers("/","/api/v1/auth/**", "/api/v1/search/**","/file/upload", "/file/upload/**").permitAll()
+            .requestMatchers("/","/api/v1/auth/**", "/api/v1/search/**","/file/upload").permitAll()
             .requestMatchers(HttpMethod.GET,"/**","/api/v1/post/**", "/api/v1/user/**").permitAll()
             .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
             .requestMatchers(HttpMethod.POST,"/**").permitAll()
